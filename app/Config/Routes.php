e9534a15e035c4c695f0da59820a2fb1cf122ee5
lucasbar28 <lucascjs49@gmail.com>
2025-09-22 21:14:49 -0300

    entidad profesores

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,3 +8,4 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 $routes->get('login', 'Login::index');
 $routes->post('auth', 'Login::auth'); 
+$routes->get('profesores', 'Profesores::index');
