8517a579f0411d71808bb2286923641ceed27fe5
lucasbar28 <lucascjs49@gmail.com>
2025-09-24 22:12:59 -0300

    -

--- app/Views/profesores_form.php
+++ app/Views/profesores_form.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agregar Profesor</title>
-    <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('css/styles.css') ?>">
 </head>
 <body>
 
